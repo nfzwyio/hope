f3e3c412c236594cb280671a748e541f0955401d
nfzwy <nfzwy@>
2024-11-07 15:43:23 +0800

    fix

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/hope/",
 
   lang: "zh-CN",
   title: "南风文档",
